91b8ffca6f61b720ace9c8917027bfd786219315
Fernando Perry <fernandoperry1234@gmail.com>
2024-11-06 19:57:58 +0700

    couple of update for broken pipe

--- serve.py
+++ serve.py
@@ -34,6 +34,9 @@ def get_script_path() -> str:
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 def html_ok(file_path:str, send_socket):
+    content_type = 'Content-Type: text/html\r\n'
+    # if 'favicon.ico' in file_path:
+    #     content_type = 'Content-Type: image/x-icon\r\n'
     f = None
     if file_path.startswith('/'):
         file_path = convert_to_web_path(file_path[1:])
@@ -47,31 +50,30 @@ def html_ok(file_path:str, send_socket):
 
     try:
         f = open(file_path)
-    except:
-        raise FileNotFoundError()
-
-    outputdata = f.read()
-    f.close()
-    send_socket.send("HTTP/1.1 200 OK\r\n".encode())
-    send_socket.send("Content-Type: text/html\r\n".encode())
-    send_socket.send("\r\n".encode())
-    send_socket.send(outputdata.encode())
+        outputdata = f.read()
+        f.close()
+        
+        send_socket.send("HTTP/1.1 200 OK\r\n".encode())
+        send_socket.send(content_type.encode())
+        send_socket.send("\r\n".encode())
+        send_socket.send(outputdata.encode())
+    except FileNotFoundError as e:
+        raise e
 
 def convert_to_web_path(path:str):
     if path.startswith("/"):
         path = path[1:]
     sc_path = get_script_path()
-    return sc_path + "/" + pconfig.web_path + "/" + path
+    full_path = sc_path + "/" + pconfig.web_path + "/" + path
+    return full_path
 
 def read_with_web_context(path) -> str:
-    content = ""
     file_path = convert_to_web_path(path)
-    f = open(file_path)
-    content = f.read()
+    with open(file_path) as f:
+        content = f.read()
     return content
 
 if __name__ == '__main__':
-
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     signal.signal(signal.SIGINT, lambda signum, frame: handle_sigint(signum, frame, serverSocket))
@@ -86,25 +88,30 @@ if __name__ == '__main__':
         
         try:
             message = connectionSocket.recv(1024).decode()
-            
-            if (len(message.split()) > 1):
-                html_ok(message.split()[1], connectionSocket)
-            
-            connectionSocket.close()
+            if len(message.split()) > 1:
+                try:
+                    html_ok(message.split()[1], connectionSocket)
+                except FileNotFoundError:
+                    content = read_with_web_context(pconfig.fof_html)
+                    connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+                    connectionSocket.send("Content-Type: text/html\r\n".encode())
+                    connectionSocket.send("\r\n".encode())
+                    connectionSocket.send(content.encode())
+                except AccessRestrictedError:
+                    content = read_with_web_context(pconfig.fot_html)
+                    connectionSocket.send("HTTP/1.1 403 Forbidden\r\n".encode())
+                    connectionSocket.send("Content-Type: text/html\r\n".encode())
+                    connectionSocket.send("\r\n".encode())
+                    connectionSocket.send(content.encode())
 
-        except AccessRestrictedError:
-            content = read_with_web_context(pconfig.fot_html)
-            connectionSocket.send("HTTP/1.1 403 Forbidden\r\n".encode())
-            connectionSocket.send("Content-Type: text/html\r\n".encode())
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.send(content.encode())
-
-        except FileNotFoundError:
+        except Exception as e:
+            print("An unexpected error occurred:", e)
             content = read_with_web_context(pconfig.fof_html)
             connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
             connectionSocket.send("Content-Type: text/html\r\n".encode())
             connectionSocket.send("\r\n".encode())
             connectionSocket.send(content.encode())
-
+        
         finally:
             connectionSocket.close()
+
